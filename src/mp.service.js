a359eeaa897ebfc6f5980dd901b15e64f4dc5f66
Rolando Gual <rolandogual@gmail.com>
2021-06-22 23:09:14 -0300

    update credentials

--- src/mp.service.js
+++ src/mp.service.js
@@ -1,7 +1,6 @@
 const mercadopago = require("mercadopago");
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 mercadopago.configure({
-  access_token: ACCESS_TOKEN,
+  access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
 });
 
 class MercadoPagoService {
